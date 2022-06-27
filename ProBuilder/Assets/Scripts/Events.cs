652c87317b3c6881964af9892503312a40338d3e
Tiago Costa <tiagodpv@gmail.com>
2022-06-26 20:12:04 -0400

    event_created

---
+++ ProBuilder/Assets/Scripts/Events.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Events : MonoBehaviour
+{
+    public UnityEvent enteredStairs;
+    public UnityEvent exitedStairs;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        enteredStairs.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        exitedStairs.Invoke();
+    }
+
+
+}
